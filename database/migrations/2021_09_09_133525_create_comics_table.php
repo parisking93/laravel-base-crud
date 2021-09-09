c4c31fccc51a19f4e0e86968b5cffd5bf7bb86ef
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-09-09 16:25:50 +0200

    add seeder

--- database/migrations/2021_09_09_133525_create_comics_table.php
+++ database/migrations/2021_09_09_133525_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title',50);
+            $table->text('img');
+            $table->text('description')->nullable();
+            $table->float('price',7,2);
+            $table->string('series',30);
+            $table->string('type',30);
+            $table->date('sale_date');
             $table->timestamps();
         });
     }
---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array_comic = config('comics');
+
+        foreach($array_comic as $comic) {
+            $newComic = new Comic();
+            $newComic->title =$comic['title'];
+            $newComic->img =$comic['thumb'];
+            $newComic->description =$comic['description'];
+            $newComic->price =$comic['price'];
+            $newComic->series =$comic['series'];
+            $newComic->type =$comic['type'];
+            $newComic->sale_date =$comic['sale_date'];
+
+            $newComic->save();
+
+
+
+
+
+
+        }
+    }
+}
